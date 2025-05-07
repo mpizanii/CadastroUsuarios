ed3e83a906fab851c8730bd71aa7791038637ac0
Matheus Pizani <mpizani28@gmail.com>
2025-05-07 12:29:39 -0300

    feat: Criei a página para redefinir senha

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage"
+import ResetPasswordPage from "./pages/auth/ResetPasswordPage"
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LoginPage/>}/>
         <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/resetpassword' element={<ResetPasswordPage/>}/>
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/auth/ResetPasswordPage.jsx
+++ frontend/src/pages/auth/ResetPasswordPage.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react"
 import { supabase } from "../../services/supabase"
 import styled from "styled-components"
-import svgAnimado from "../../assets/svgAnimado.svg"
-import { Link } from "react-router-dom";
+import svgAnimadoRedefinirSenha from "../../assets/svgAnimadoRedefinirSenha.svg"
 
 const Container = styled.div`
     width: 100vw;
@@ -88,79 +87,58 @@ const Message = styled.div`
 `;
 
 export default function LoginPage() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+    const [repeatNewPassword, setRepeatNewPassword] = useState("")
     const [message, setMessage] = useState("")
     const [messageType, setMessageType] = useState("success")
 
-    async function handleSignIn() {
-        if (!email || !password) {
-            setMessageType("error");
-            setMessage("Preencha todos os campos.");
-            return;
+    async function handleSubmit(e) {
+        e.preventDefault();
+      
+        if (newPassword !== repeatNewPassword) {
+          setMessageType("error");
+          setMessage("As senhas não coincidem.");
+          return;
         }
-
-        const {
-            data: {session},
-            error,
-        } = await supabase.auth.signInWithPassword({email, password})
-        if (error){
-            setMessageType("error")
-            setMessage("Erro ao entrar: " + error.message)
-            return;
-        } 
-        if (session){
-            setMessageType("success")
-            setMessage("Login realizado com sucesso!")
+      
+        if (!newPassword || !repeatNewPassword) {
+          setMessageType("error");
+          setMessage("Preencha todos os campos.");
+          return;
         }
-    }
-    async function handleForgotPassword() {
-        if (!email) {
-            setMessageType("error");
-            setMessage("Informe seu e-mail para redefinir a senha.");
-            return;
-        }
-
-        const {data, error} = await supabase.auth.resetPasswordForEmail(email, {
-            redirectTo: "http://localhost:5173/resetpassword"
-        });
+      
+        const { error } = await supabase.auth.updateUser({ password: newPassword });
+      
         if (error) {
-            setMessageType("error");
-            setMessage("Erro ao solicitar redefinição de senha: " + error.message);
-            return;
+          setMessageType("error");
+          setMessage("Erro: " + error.message);
         } else {
-            setMessageType("success");
-            setMessage("Verifique seu e-mail, enviamos um e-mail para redefinição de senha!");
+          setMessageType("success");
+          setMessage("Senha redefinida com sucesso.");
+          setNewPassword("");
+          setRepeatNewPassword("");
         }
-    }
-
+      }
+      
     return (
         <Container>
             <Card>
                 <LeftCard>
-                    <h1>Bem vindo novamente!</h1>
-                    <p>Faça login com o seu e-mail.</p>
-                    <img src={svgAnimado} alt="svg" style={{ width: "300px" }} />
+                    <h1>Redefinição de senha!</h1>
+                    <img src={svgAnimadoRedefinirSenha} alt="svg" style={{ width: "450px" }} />
                     <a href="https://storyset.com/business" style={{ fontSize: "12px" }}>Business illustrations by Storyset</a>
                 </LeftCard>
                 <RightCard>
-                    <h1>Login</h1>
+                    <h1>Redefinir Senha</h1>
                     <InputDiv style={{ marginTop: "20px" }}>
-                        <Icon className="bi bi-envelope" />
-                        <RightCardInput type="email" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                        <Icon className="bi bi-lock-fill" />
+                        <RightCardInput type="password" placeholder="Nova Senha" onChange={(e) => setNewPassword(e.target.value)} value={newPassword}/>
                     </InputDiv>
                     <InputDiv style={{ marginTop: "10px" }}>
                         <Icon className="bi bi-lock-fill" />
-                        <RightCardInput type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password}/>
+                        <RightCardInput type="password" placeholder="Repetir Nova Senha" onChange={(e) => setRepeatNewPassword(e.target.value)} value={repeatNewPassword}/>
                     </InputDiv>
-                    <Button onClick={handleSignIn}>Entrar</Button>
-                    <a href="#" style={{ marginTop: "10px", cursor: "pointer" }} onClick={(e) => {
-                        e.preventDefault()
-                        handleForgotPassword();
-                        }}>
-                        Esqueceu a senha?
-                    </a>
-                    <p style={{ marginTop: "10px" }}>Não possui cadastro? <Link to="/register">Cadastre-se</Link></p>
+                    <Button onClick={handleSubmit}>Confirmar</Button>
                     {message && <Message type={messageType}>{message}</Message>}
                 </RightCard>
             </Card>
