656ff208378451e09fc51aa93413ee4ebe47690d
Matheus Pizani <mpizani28@gmail.com>
2025-05-14 20:39:14 -0300

    feat: Criei o form de adicionar usuários ao banco de dados

--- frontend/src/pages/usersPage/StyledUsersPage.js
+++ frontend/src/pages/usersPage/StyledUsersPage.js
@@ -8,6 +8,11 @@ export const Container = styled.div`
     align-items: center;
     justify-content: flex-end;
     font-family: Roboto, sans-serif;
+
+    &.blur{
+        filter: blur(2px);
+        transition: filter 0.5s ease;
+    }
 `;
 
 export const Card = styled.div`
@@ -76,4 +81,78 @@ export const TableHeader = styled.div`
     background-color: #a9a6a6;
     padding: 0px 5px;
     color: white
+`;
+
+export const DivFormAddUser = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    font-family: Roboto, sans-serif;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    opacity: ${prop => prop.$visible ? 1 : 0};
+    pointer-events: ${prop => prop.$visible ? 'auto' : 'none'};
+    transition: all 0.5 ease;
+`;
+
+export const FormAddUser = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #162521;
+    color: white;
+    border-radius: 10px;
+    width: 30%;
+    height: 60%;
+    padding: 10px;
+    gap: 10px;
+    opacity: ${prop => prop.$visible ? 1 : 0};
+    pointer-events: ${prop => prop.$visible ? 'auto' : 'none'};
+    transition: all 0.5s ease;
+    box-shadow: -2px 6px 4px 0px rgba(0, 0, 0, 0.47);
+`;
+
+export const FormAddUserInput = styled.input`
+    display: flex;
+    width: 100%;
+    height: 20px;
+    border-radius: 10px;
+    border: none;
+    padding: 5px;
+    outline: none;
+`;
+
+export const FormAddUserCancelButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    width: 40%;
+    height: 20px;
+    background-color: red;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+`;
+
+export const FormAddUserSubmitButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    width: 40%;
+    height: 20px;
+    background-color: green;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+`;
+
+export const Message = styled.div`
+  margin-top: 20px;
+  color: ${(props) => (props.type === "success" ? "green" : "red")};
 `;
--- frontend/src/pages/usersPage/UsersPage.jsx
+++ frontend/src/pages/usersPage/UsersPage.jsx
@@ -1,31 +1,161 @@
-import { Container, Card, CardButtons, SearchWrapper, SearchButton, SearchInput, TableHeader, AddUserButton } from "./StyledUsersPage";
+import {
+  Container,
+  Card,
+  CardButtons,
+  SearchWrapper,
+  SearchButton,
+  SearchInput,
+  TableHeader,
+  AddUserButton,
+  DivFormAddUser,
+  FormAddUser,
+  FormAddUserInput,
+  FormAddUserCancelButton,
+  FormAddUserSubmitButton,
+  Message
+} from "./StyledUsersPage";
+
 import MenuLateral from "../../components/menu/menulateral";
+import { useState } from "react";
+import { supabase } from "../../services/supabase";
 
 export default function UsersPage() {
+  const [menuAddUserAtivo, setMenuAddUserAtivo] = useState(false);
+  const [formAddUser, setFormAddUser] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("success");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
 
-    return (
-        <Container>
-            <MenuLateral />
-            <Card>
-                <h2 style={{ marginBottom: "20px" }}>Clientes</h2>
-                <CardButtons>
-                    <SearchWrapper>
-                        <SearchInput type="text" placeholder="Buscar Nome" />
-                        <SearchButton>
-                            <i className="bi bi-search"></i>
-                        </SearchButton>
-                    </SearchWrapper>
-                    <AddUserButton>Adicionar Cliente</AddUserButton>
-                </CardButtons>
-                <TableHeader>
-                    <h4>ID</h4>
-                    <h4>Nome</h4>
-                    <h4>E-mail</h4>
-                    <h4>Telefone</h4>
-                    <h4>Endereço</h4>
-                    <h4>Ações</h4>
-                </TableHeader>
-            </Card>
-        </Container>
-    );
+  async function AddUser(e) {
+    e.preventDefault();
+
+    const { error } = await supabase
+      .from('users')
+      .insert([
+        {
+          nome: name,
+          email: email,
+          telefone: phone,
+          endereco: address
+        }
+      ]);
+
+    if (error) {
+      setMessageType("error");
+      setMessage("Erro: " + error.message);
+    } else {
+      setMessageType("success");
+      setMessage("Cliente adicionado com sucesso.");
+      setName("");
+      setEmail("");
+      setPhone("");
+      setAddress("");
+
+      setTimeout(() => {
+        setFormAddUser(false);
+        setMenuAddUserAtivo(false);
+        window.location.reload();
+      }, 2000);
+    }
+  }
+
+  return (
+    <>
+      <Container className={menuAddUserAtivo ? "blur" : ""}>
+        <MenuLateral />
+        <Card>
+          <h2 style={{ marginBottom: "20px" }}>Clientes</h2>
+          <CardButtons>
+            <SearchWrapper>
+              <SearchInput type="text" placeholder="Buscar E-mail" />
+              <SearchButton>
+                <i className="bi bi-search"></i>
+              </SearchButton>
+            </SearchWrapper>
+            <AddUserButton onClick={() => {
+              setMenuAddUserAtivo(true);
+              setFormAddUser(true);
+            }}>
+              Adicionar Cliente
+            </AddUserButton>
+          </CardButtons>
+          <TableHeader>
+            <h4>ID</h4>
+            <h4>Nome</h4>
+            <h4>E-mail</h4>
+            <h4>Telefone</h4>
+            <h4>Endereço</h4>
+            <h4>Ações</h4>
+          </TableHeader>
+        </Card>
+      </Container>
+
+      <DivFormAddUser $visible={formAddUser}>
+        <FormAddUser $visible={formAddUser} onSubmit={AddUser}>
+          <h1>Cadastrar Cliente</h1>
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            width: "60%",
+            gap: "5px"
+          }}>
+            <label htmlFor="name" style={{ marginLeft: "5px" }}>Nome</label>
+            <FormAddUserInput
+              id="name"
+              type="text"
+              placeholder="Obrigatório"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+
+            <label htmlFor="email" style={{ marginLeft: "5px" }}>E-mail</label>
+            <FormAddUserInput
+              id="email"
+              type="email"
+              placeholder="Opcional"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <label htmlFor="phone" style={{ marginLeft: "5px" }}>Telefone</label>
+            <FormAddUserInput
+              id="phone"
+              type="text"
+              placeholder="Opcional"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+
+            <label htmlFor="address" style={{ marginLeft: "5px" }}>Endereço</label>
+            <FormAddUserInput
+              id="address"
+              type="text"
+              placeholder="Opcional"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+
+            <div style={{ display: "flex", justifyContent: "space-evenly", marginTop: "5px" }}>
+              <FormAddUserSubmitButton type="submit">Confirmar</FormAddUserSubmitButton>
+              <FormAddUserCancelButton type="button" onClick={() => {
+                setFormAddUser(false);
+                setMenuAddUserAtivo(false);
+              }}>
+                Cancelar
+              </FormAddUserCancelButton>
+            </div>
+
+            <div style={{ display: "flex", justifyContent: "center", marginTop: "5px" }}>
+              {message && <Message type={messageType}>{message}</Message>}
+            </div>
+          </div>
+        </FormAddUser>
+      </DivFormAddUser>
+    </>
+  );
 }
