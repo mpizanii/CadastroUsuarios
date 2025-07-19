4a6b967ada0b458fca26fc5d572aff88c387a9fb
Matheus Pizani <mpizani28@gmail.com>
2025-07-19 11:37:35 -0300

    criando endpoint patch e delete para clientes

--- api/Controllers/ClientesController.cs
+++ api/Controllers/ClientesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.DTOs;
 using api.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -24,14 +25,29 @@ namespace api.Controllers
         [HttpGet]
         public async Task<IActionResult> ObterTodos()
         {
-            var clientes = await _context.Clientes.ToListAsync();
+            var clientes = await _context.Clientes.Select(c => new ClientesDTO
+            {
+                Nome = c.Nome,
+                Email = c.Email,
+                Telefone = c.Telefone,
+                Endereco = c.Endereco
+            }).ToListAsync();
+
             return Ok(clientes);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> ObterPorId(int id)
         {
-            var cliente = await _context.Clientes.FirstOrDefaultAsync(c => c.Id == id);
+            var cliente = await _context.Clientes
+            .Where(c => c.Id == id)
+            .Select(c => new ClientesDTO
+            {
+                Nome = c.Nome,
+                Email = c.Email,
+                Telefone = c.Telefone,
+                Endereco = c.Endereco
+            }).FirstOrDefaultAsync();
 
             if (cliente == null)
             {
@@ -46,7 +62,50 @@ namespace api.Controllers
         {
             _context.Clientes.Add(cliente);
             await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(ObterPorId), new { id = cliente.Id}, cliente);
+            return CreatedAtAction(nameof(ObterPorId), new { id = cliente.Id }, cliente);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> EditarCliente(int id, [FromBody] ClientesDTO clienteDTO)
+        {
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            if (!string.IsNullOrWhiteSpace(clienteDTO.Nome) && clienteDTO.Nome != "string")
+                cliente.Nome = clienteDTO.Nome;
+
+            if (!string.IsNullOrWhiteSpace(clienteDTO.Email) && clienteDTO.Email != "string")
+                cliente.Email = clienteDTO.Email;
+
+            if (!string.IsNullOrWhiteSpace(clienteDTO.Telefone) && clienteDTO.Telefone != "string")
+                cliente.Telefone = clienteDTO.Telefone;
+
+            if (!string.IsNullOrWhiteSpace(clienteDTO.Endereco) && clienteDTO.Endereco != "string")
+                cliente.Endereco = clienteDTO.Endereco;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(clienteDTO);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarCliente(int id)
+        {
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+
+            return Ok(cliente);
         }
     }
 }
