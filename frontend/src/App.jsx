58afa5b9a8b4d08e9064bdcaed34c898c2809c6d
mpizanii <mpizani28@gmail.com>
2025-11-19 14:25:38 -0300

    adicionando sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import LoginPage from "./pages/auth/LoginPage";
 import ResetPasswordPage from "./pages/auth/ResetPasswordPage"
 import UsersPage from './pages/usersPage/UsersPage';
@@ -10,6 +11,7 @@ import ProtectedRoutes from './services/protectedroutes';
 import NavbarSuperior from './components/menu/Navbar';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SideBar from './components/menu/Sidebar';
 
 function App() {
   return (
@@ -21,22 +23,56 @@ function App() {
 
 function Layout() {
   const location = useLocation();
+  const navigate = useNavigate();
   const path = location.pathname;
+  const [activeItem, setActiveItem] = useState('');
 
   const rotasSemNavbar = ["/", "/resetpassword"];
 
+  const routeToIdMap = {
+    '/metricas': 'visao-geral',
+    '/pedidos': 'pedidos',
+    '/produtos': 'produtos',
+    '/receitas': 'receitas',
+    '/estoque': 'estoque',
+    '/clientes': 'clientes',
+    '/suporte': 'suporte'
+  };
+
+  useEffect(() => {
+    setActiveItem(routeToIdMap[path] || '');
+  }, [path]);
+
+  const handleSidebarItemClick = (item) => {
+    setActiveItem(item.id);
+    navigate(item.path);
+  };
+
   return (
     <>
-      {!rotasSemNavbar.includes(path) && <NavbarSuperior path={path} />}
-      <Routes>
-        <Route path='/' element={<LoginPage/>}/>
-        <Route path='/resetpassword' element={<ResetPasswordPage/>}/>
-        <Route path='/clientes' element={<ProtectedRoutes><UsersPage/></ProtectedRoutes>}/>
-        <Route path='/produtos' element={<ProtectedRoutes><ProductsPage/></ProtectedRoutes>}/>
-        <Route path='/estoque' element={<ProtectedRoutes><StockPage/></ProtectedRoutes>}/>
-        <Route path='/pedidos' element={<ProtectedRoutes><OrdersPage/></ProtectedRoutes>}/>
-        <Route path='/metricas' element={<ProtectedRoutes><DashboardPage/></ProtectedRoutes>}/>
-      </Routes>
+      {!rotasSemNavbar.includes(path) && (
+        <>
+          <SideBar 
+            activeItem={activeItem} 
+            onItemClick={handleSidebarItemClick}
+          />
+          <div style={{ marginLeft: '280px' }}>
+            <Routes>
+              <Route path='/clientes' element={<ProtectedRoutes><UsersPage/></ProtectedRoutes>}/>
+              <Route path='/produtos' element={<ProtectedRoutes><ProductsPage/></ProtectedRoutes>}/>
+              <Route path='/estoque' element={<ProtectedRoutes><StockPage/></ProtectedRoutes>}/>
+              <Route path='/pedidos' element={<ProtectedRoutes><OrdersPage/></ProtectedRoutes>}/>
+              <Route path='/metricas' element={<ProtectedRoutes><DashboardPage/></ProtectedRoutes>}/>
+            </Routes>
+          </div>
+        </>
+      )}
+      {rotasSemNavbar.includes(path) && (
+        <Routes>
+          <Route path='/' element={<LoginPage/>}/>
+          <Route path='/resetpassword' element={<ResetPasswordPage/>}/>
+        </Routes>
+      )}
     </>
   );
 }
